f4057dabe128a5bb9fe7f85801336bcc678f493d
Verdient <fagui.zhu@foxmail.com>
2024-03-11 11:27:55 +0800

    新增生成密码

--- src/Password.php
+++ src/Password.php
@@ -13,10 +13,34 @@ use function Hyperf\Config\config;
 class Password
 {
     /**
-     * @var string 哈希算法
+     * 哈希算法
      * @author Verdient。
      */
     const ALGO = PASSWORD_DEFAULT;
+
+    /**
+     * 小写字母
+     * @author Verdient。
+     */
+    const CHARSET_LOWER_ALPHA = 'abcdefghijklmnopqrstuvwxyz';
+
+    /**
+     * 大写字母
+     * @author Verdient。
+     */
+    const CHARSET_UPPER_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    /**
+     * 数字
+     * @author Verdient。
+     */
+    const CHARSET_NUMERIC = '0123456789';
+
+    /**
+     * 特殊符号
+     * @author Verdient。
+     */
+    const CHARSET_SPECIAL = '!@$%&';
 
     /**
      * 生成密码哈希
@@ -47,5 +71,40 @@ class Password
             return password_verify($password, $hash);
         }
         return false;
+    }
+
+    /**
+     * 生成密码
+     * @param int $length 密码长度
+     * @param array $charsets 使用的字符集
+     * @author Verdient。
+     */
+    public static function generate(int $length = 16, array $charsets = [])
+    {
+        $charsets = array_values($charsets);
+
+        if (empty($charsets)) {
+            $charsets = [
+                static::CHARSET_LOWER_ALPHA,
+                static::CHARSET_UPPER_ALPHA,
+                static::CHARSET_NUMERIC,
+                static::CHARSET_SPECIAL
+            ];
+        }
+
+        $max = count($charsets) - 1;
+
+        $result = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $charSetIndex = $max === 0 ? 0 : random_int(0, $max);
+            $charset = $charsets[$charSetIndex];
+
+            $index = random_int(0, strlen($charset) - 1);
+
+            $result .= $charset[$index];
+        }
+
+        return $result;
     }
 }
